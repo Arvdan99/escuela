50e4b3596d8e5d67202d0a681d0337cd26c045b9
Alumno ULSA <alumno@D505.local>
2023-03-01 14:26:21 -0700

    Commit tres

--- app/Http/Controllers/AlumnosController.php
+++ app/Http/Controllers/AlumnosController.php
@@ -10,9 +10,27 @@ class AlumnosController extends Controller
     public function index(){
         $alumnos = Alumno::all();
 
-        $argumanetos = array();
-        $argumanetos['alumnos'] = $alumnos;
+        $argumentos = array();
+        $argumentos['alumnos'] = $alumnos;
 
-        return  view('alumnos.index', $argumanetos);
+        return  view('alumnos.index', $argumentos);
     }
+
+    public function create (){
+        $argumentos = array();
+        return view('alumnos.create', $argumentos);
+    }
+
+    public function store(Request $request){
+        $nuevoAlumno = new Alumno();
+        //Las columnos de la tabla asociada 
+        //Se representan mediante propiedades del objeto 
+        $nuevoAlumno->nombre = $request->input('nombre');
+        $nuevoAlumno->save();
+        //Hacer que nos regrese al index
+        return redirect()->route('alumnos.index');
+    }
+
 }
+
+
